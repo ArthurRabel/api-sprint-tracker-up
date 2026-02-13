c45329a7fb8d2277d9bc79a12f90e9f234109a2c
Arthur Rabel <arthur.rabel@outlook.com>
2026-02-13 17:11:23 -0300

    feat(auth): restructure auth types and export from common interfaces

--- src/common/enums/task-status.enum.ts
+++ src/common/enums/task-status.enum.ts
@@ -1,4 +1,4 @@
-export enum TaskStatus {
+export enum Status {
   TODO = 'TODO',
   IN_PROGRESS = 'IN_PROGRESS',
   DONE = 'DONE',
---
+++ src/common/interfaces/index.ts
@@ -0,0 +1,2 @@
+export { AuthenticatedUser } from './user.interface';
+export { User, AuthProvider, Role } from './user.types';
